d9ded82cb88b7f7faf8f8a23b3a6b4922d0ecb16
Slmii <selami.c@sbytes-it.com>
2024-12-22 13:40:37 +0100

    Add legacy plugin to Vite configuration for better browser support

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
+import legacy from '@vitejs/plugin-legacy';
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		legacy({
+			targets: ['>0.2%', 'not dead', 'not op_mini all', 'defaults', 'not IE 11']
+		})
+	],
 	resolve: {
 		alias: {
 			'@': path.resolve('./src')
@@ -14,5 +20,4 @@ export default defineConfig({
 		__APP_ENV__: process.env.VITE_VERCEL_ENV
 	},
 	base: './'
-	// server: { host: '127.0.0.1', port: 81 }
 });
